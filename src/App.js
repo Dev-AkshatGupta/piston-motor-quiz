5b3eaab675d18373101783eec4190b5f8d8a6755
akshatgupta105 <53713926+Dev-AkshatGupta@users.noreply.github.com>
2022-05-06 13:59:18 +0530

    Initial commit for the  project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Routes,Route} from "react-router-dom";
+import {LogInForm} from "Components/Authentication/LogInForm";
+import {SignUpForm} from "Components/Authentication/SignUpForm";
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
